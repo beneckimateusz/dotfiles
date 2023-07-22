01153fddc4fc3f6d80b80f3fb4eeda4d8e5d03d0
Mateusz Benecki <benecki.mateusz@gmail.com>
2023-07-23 01:02:02 +0200

    Make TailwindCSS LSP work with HEEx

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,6 +3,17 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       autoformat = false,
+
+      servers = {
+        tailwindcss = {
+          init_options = {
+            userLanguages = {
+              elixir = "html",
+              heex = "html",
+            },
+          },
+        },
+      },
     },
   },
 }
